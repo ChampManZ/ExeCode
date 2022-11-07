80bf076eb30ca0872bc256068953da711cdaf1fc
wan-kcn <63011181@kmitl.ac.th>
2022-11-07 08:55:15 +0700

    Home Announcement

--- execode-frontend/src/pages/Home.js
+++ execode-frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material"
 import Sidebar from "../components/Sidebar"
 import TodoList from "../components/Home/TodoList"
+import Announce from "../components/Home/Announce"
 import { ThemeProvider } from "@emotion/react"
 import theme from "../components/theme"
 
@@ -17,6 +18,7 @@ function Home () {
 
             <Box sx={{ mt:10 }}>
             <h1>Announcement</h1>
+            <Announce></Announce>
             </Box>
         </Box>
         </>
